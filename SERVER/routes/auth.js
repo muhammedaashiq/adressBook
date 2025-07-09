f9e5cf95e9542b8077fb43ca08cabf1f9d0a2a86
Muhammed Aashiq <aashiqmuhammed007@gmail.com>
2025-07-09 21:07:26 +0530

    second Commit

--- SERVER/routes/auth.js
+++ SERVER/routes/auth.js
@@ -1,11 +1,48 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt')
+
+
 const User = require("../models")
+
+
 router.post("/login")
 
-router.post("/register",(req,res) => {
-    try{
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
 
-    }catch(err){
+    if (!name || !email || !password)
+        return res
+            .status(400)
+            .json({ error: 'please enter all the required field.' })
+
+    const emailReg = '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+
+    if (name.length >= 25)
+        return res
+            .status(400)
+            .json({ error: "name can only be less than 25 characters." })
+
+
+    if (!emailReg.test(email))
+        return res
+            .status(400)
+            .json({ error: 'Please enter a valid email address' })
+
+
+    if (password.length <= 6)
+        return res
+            .status(400)
+            .json({ error: "password must be atleaset 6 characters." })
+
+    try {
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const newUser = new User({ name, email, password: hashedPassword })
+
+        const result = await newUser.save()
+
+        return res.status(201).json({...result._doc})
+    } catch (err) {
         console.error(err);
         return res.status(500).json({ error: err.messsage })
     }
